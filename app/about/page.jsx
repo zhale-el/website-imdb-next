d2bb7d239091b159c3f1dfd943c99554176cf586
zhaleh <zhalehelmi@gmail.com>
2024-06-04 20:52:03 +0330

    create about page

---
+++ app/about/page.jsx
@@ -0,0 +1,37 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus saepe
+        itaque quibusdam nisi necessitatibus dignissimos ullam suscipit
+        reiciendis ipsum est! Tenetur nemo culpa explicabo, a dignissimos
+        repellat
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque rem id
+        quaerat, sed optio dicta eaque suscipit explicabo. Laudantium, inventore
+        praesentium. Odio, rerum fugiat ullam vitae eius iure culpa quisquam
+        expedita sunt animi quis eaque iusto perferendis aliquid fuga natus sit
+        quod facere optio. Optio ea eligendi veritatis nostrum perferendis ullam
+        iure ut eveniet illum voluptates adipisci quidem laboriosam, nisi dolore
+        aperiam impedit sunt voluptatum, nesciunt minima at ipsum dolor
+        similique neque! Quo, animi perferendis, voluptatem excepturi eos nam
+        commodi quos aliquid magni, facilis temporibus mollitia autem. Inventore
+        possimus ut voluptatem, quos amet magnam voluptas excepturi vero ipsa,
+        doloribus beatae.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error minus
+        nulla voluptas deserunt quidem, doloremque sint vitae ratione numquam
+        consequatur, molestias tempora eius nemo excepturi voluptates suscipit
+        sapiente perferendis saepe! Non iste magni quia dolorum nostrum
+        laudantium esse vel voluptatibus magnam eveniet distinctio consequatur,
+        numquam nesciunt a veritatis eius deleniti alias saepe quo corporis nisi
+        ea pariatur aspernatur cupiditate? Perferendis.
+      </p>
+    </div>
+  );
+};
+
+export default About;
